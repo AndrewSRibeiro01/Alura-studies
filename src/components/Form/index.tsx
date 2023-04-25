41b42331654d10c1e50ed4c72624be1773cddddc
AndrewSRB <116013992+AndrewSRibeiro01@users.noreply.github.com>
2023-04-25 10:16:08 -0300

    ok

---
+++ src/components/Form/index.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+import Botão from "../Button";
+
+class Form extends React.Component {
+    render() {
+        return (
+            <form>
+                <div>
+                    <label htmlFor="tarefa">
+                        Adicione um novo estudo
+                    </label>
+                    <input
+                        type="text"
+                        name="tarefa"
+                        id="tarefa"
+                        placeholder="Oque você quer estudar"
+                        required />
+                </div>
+                <div>
+                    <label htmlFor="tempo">
+                        Tempo
+                    </label>
+                    <input
+                        type="time"
+                        step="1"
+                        name="tempo"
+                        id="tempo"
+                        min="00:00:00"
+                        max="01:30:00"
+                        required />
+                </div>
+                <Botão />
+            </form>
+        )
+
+    }
+}
+
+export default Form 
---
+++ src/components/Lista/index.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+function Lista() {
+    return (
+        <aside>
+            <h2> Estudos do dia </h2>
+            <ul>
+                <li>
+                    <h3></h3>
+                </li>
+                <li>
+                    <h3></h3>
+                </li>
+            </ul>
+        </aside>
+    )
+}
+
+export default Lista;
